7a300053dbab6c4b974fb9126adc93932828c6f8
natumn <natsume.pcuse75@gmail.com>
2019-01-17 10:33:06 +0900

    add playground

---
+++ dentaku/dentaku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dentaku
+//
+//  Created by 棗光幹 on 2019/01/17.
+//  Copyright © 2019 natumn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
